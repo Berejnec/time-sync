e0c9c26c0458e688b34b34c8cfa3c9c7181c9d56
Adrian Berejnec <adrianberejnec29@gmail.com>
2024-11-03 11:04:25 +0200

    Setup Clerk Auth, Drizzle Postgres DB, protecting routes, Shadcn Tailwind

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+  return (
+    <h1 className="min-h-screen flex flex-col justify-center items-center">
+      404 | Page was not found.
+    </h1>
+  );
+}
